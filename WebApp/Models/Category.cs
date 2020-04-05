629d10c6e2ec15a0bb8e6f8322567516334fd9b6
new <new@LENOVO>
2020-04-05 20:07:16 +0200

    Add project files.

---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "NumberOfProduct")]
+        public int NumberOfProduct { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ WebApp/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        [DisplayName("Product Price")]
+        public int Price { get; set; }
+        [DisplayName("Product Image")]
+        public string Image { get; set; }
+        [DisplayName("Product Description")]
+        public string Description { get; set; }
+        [DisplayName("Product Category")]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+}
